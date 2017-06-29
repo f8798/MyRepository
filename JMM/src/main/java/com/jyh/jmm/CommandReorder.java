3714e7a36b25538392146cf9346d570606c3b671
jinyunhui <f8798@163.com>
2017-06-29 16:28:31 +0800

    2222

--- JMM/src/main/java/com/jyh/jmm/CommandReorder.java
+++ JMM/src/main/java/com/jyh/jmm/CommandReorder.java
@@ -1,33 +1,12 @@
 package com.jyh.jmm;
 
 public class CommandReorder {
-	public static int a = 0;
-	public static int b = 0;
 	public static void main(String[] args) throws InterruptedException {
 		int i = 0;
 		while(i++ <1000000){
-			a = b = 0;
-			Thread t1 = new Thread(new Runnable() {
-				public void run() {
-					a = 1;
-					b = 2;
-				}
-			});
-			
-			Thread t2 = new Thread(new Runnable() {
-				public void run() {
-					if(a == 0 && b == 2){
-						System.out.println("reorder happened!");
-					}else if(a == 0 && b == 0){
-						
-						System.out.println("reorder happened!");
-					}else{
-						System.out.println(a +  " " + b);
-					}
-						
-					
-				}
-			});
+			TestObject2 to2 = new TestObject2();
+			Thread1 t1 = new Thread1(to2);
+			Thread2 t2 = new Thread2(to2);
 			
 			t1.start();
 			t2.start();
@@ -36,5 +15,49 @@ public class CommandReorder {
 		}
 
 	}
+	
+	static class TestObject{
+		public void invoke(){
+//			System.out.println("invoked!");
+		}
+	}
+	
+	static class TestObject2{
+		public boolean a = false;
+		public TestObject b;
+	}
+	
+	static class Thread1 extends Thread{
+		TestObject2 to2;
+		public Thread1(TestObject2 to2){
+			this.to2 = to2;
+		}
+		public void run() {
+			
+			if(to2.a==true ){
+				
+				to2.b.invoke();
+			}else{
+//				System.out.println(to2.a + "  " + to2.b);
+			}
+		}
+	}
+	
+	static class Thread2 extends Thread{
+		TestObject2 to2;
+		public Thread2(TestObject2 to2){
+			this.to2 = to2;
+		}
+		public void run() {
+			init();
+			to2.a = true;
+		}
+		
+		void init(){
+			to2.b = new TestObject();
+		}
+	}
 
 }
+
+
