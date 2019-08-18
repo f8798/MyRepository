cd87b849a4fca5e19aed003bccfd4119dd0bfda7
f8798 <f8798@163.com>
2019-08-18 21:11:25 +0800

    Dubbo study

--- netty/src/main/java/com/test/echo/handler/EchoServerHandler.java
+++ netty/src/main/java/com/test/echo/handler/EchoServerHandler.java
@@ -3,11 +3,12 @@ package com.test.echo.handler;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.CharsetUtil;
 
-
+@ChannelHandler.Sharable
 public class EchoServerHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
